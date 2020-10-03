3a57b67d7f59b46b0e9f44e1712c89bd96e07fb8
Mai Duy Vương <37338116+1560693@users.noreply.github.com>
2020-10-03 19:09:14 +0700

    Add files via upload

---
+++ js/responsive.js
@@ -0,0 +1,12 @@
+// menu
+function myFunction() {
+  var x = document.getElementById("my-navbar");
+  if (x.className === "navbar") 
+  {
+      x.className += " responsive";
+  } 
+  else 
+  {
+      x.className = "navbar";
+  }
+}
